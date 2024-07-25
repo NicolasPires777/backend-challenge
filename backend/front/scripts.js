c6da2eef3df7c0d4699388f330149e85039b7aba
NicolasPires777 <nicolas@zedia.com.br>
2024-07-25 18:52:04 -0300

    correction on the front to return error to the user and give feedback when loading

--- backend/front/scripts.js
+++ backend/front/scripts.js
@@ -1,6 +1,10 @@
-function callapi(event){
+function callapi(event) {
     event.preventDefault();
     
+    // Mostrar a mensagem de carregamento
+    const loadingElement = document.getElementById('loading-message');
+    loadingElement.style.display = 'block';
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
@@ -20,12 +24,35 @@ function callapi(event){
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())
+    .then(response => {
+        // Ocultar a mensagem de carregamento
+        loadingElement.style.display = 'none';
+        
+        if (!response.ok) {
+            return response.json().then(errorData => {
+                // Atualiza o texto do elemento de erro
+                const errorElement = document.getElementById('error-message');
+                errorElement.textContent = errorData.detail || 'Ocorreu um erro ao enviar a mensagem.';
+                errorElement.style.display = 'block';  // Mostra o elemento de erro
+                document.getElementById('success-message').style.display = 'none'; // Oculta o elemento de sucesso
+                throw new Error(errorData.detail || 'Ocorreu um erro ao enviar a mensagem.');
+            });
+        }
+        return response.json();
+    })
     .then(data => {
-        alert('Mensagem enviada com sucesso!');
+        // Atualiza o texto do elemento de sucesso
+        const successElement = document.getElementById('success-message');
+        successElement.textContent = 'Solicitação enviada, verifique seu e-mail';
+        successElement.style.display = 'block';  // Mostra o elemento de sucesso
+        document.getElementById('error-message').style.display = 'none'; // Oculta o elemento de erro
     })
     .catch(error => {
         console.error('Erro:', error);
-        alert(`Ocorreu um erro ao enviar a mensagem: ${error.message}`);
+        // Caso o erro não seja tratado pelo bloco .then, atualiza o texto do elemento de erro
+        const errorElement = document.getElementById('error-message');
+        errorElement.textContent = error.message || 'Ocorreu um erro ao enviar a mensagem.';
+        errorElement.style.display = 'block';  // Mostra o elemento de erro
+        document.getElementById('success-message').style.display = 'none'; // Oculta o elemento de sucesso
     });
-};
+}
